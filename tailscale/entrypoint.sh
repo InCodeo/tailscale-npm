33bc8ca9096e267257a35602c33beba5716b2cf2
InCodeo <githublife@proton.me>
2024-11-30 12:14:23 +1100

    v2

--- tailscale/entrypoint.sh
+++ tailscale/entrypoint.sh
@@ -2,9 +2,9 @@
 
 # Check if the Tailscale auth key is provided
 if [ -z "$TAILSCALE_AUTH_KEY" ]; then
-  echo "TAILSCALE_AUTH_KEY environment variable is not set. Exiting."
-  exit 1
+    echo "TAILSCALE_AUTH_KEY environment variable is not set. Exiting."
+    exit 1
 fi
 
-# Start supervisord to manage tailscale
+# Start supervisord to manage tailscale and nginx
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
